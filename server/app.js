ebb06d934ebf8a73ac1c52be8a2b84008e4d9220
Fahd Ahsan <sfahsan@umich.edu>
2022-11-30 17:52:51 -0500

    part 2 done

--- server/app.js
+++ server/app.js
@@ -1,18 +1,19 @@
 const express = require('express');
 const app = express();
 
-
 require('express-async-errors');
-// const User = require('./models/user');
-
-// app.get('/users', async (req, res) => {
-//   const users = await User.findAll();
-//   res.send(users);
-// });
-
 app.use(express.json());
-
 app.use('/static', express.static('assets'))
+
+app.use("/", (req, res, next) => {
+  console.log(req.method)
+  console.log(req.url)
+  res.on('finish', () => {
+    // read and log the status code of the response
+    console.log(res.statusCode)
+  });
+  next()
+})
 
 // For testing purposes, GET /
 app.get('/', (req, res) => {
@@ -32,6 +33,23 @@ app.post('/test-json', (req, res, next) => {
 app.get('/test-error', async (req, res) => {
   throw new Error("Hello World!")
 });
+
+app.use((req, res, next) => {
+  let err = new Error("Sorry, the requested resource couldn't be found")
+  err.statusCode = 404
+  next(err)
+})
+
+app.use((err, req, res, next) => {
+  // console.error(err)
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Error"
+  res.status(statusCode)
+  res.json({
+    message,
+    statusCode
+  })
+})
 
 const port = 5000;
 app.listen(port, () => console.log('Server is listening on port', port));
