53b1f62ff992f5761fe93963219a946b4576f392
Ye Lwin Htay <yelwin.r.k@gmail.com>
2022-11-30 14:33:08 -0800

    boilerplate done

--- server/app.js
+++ server/app.js
@@ -1,10 +1,24 @@
 const express = require('express');
 const app = express();
 
+
+require('express-async-errors');
+// const User = require('./models/user');
+
+// app.get('/users', async (req, res) => {
+//   const users = await User.findAll();
+//   res.send(users);
+// });
+
+app.use(express.json());
+
+app.use('/static', express.static('assets'))
+
 // For testing purposes, GET /
 app.get('/', (req, res) => {
   res.json("Express server running. No content provided at root level. Please use another route.");
 });
+
 
 // For testing express.json middleware
 app.post('/test-json', (req, res, next) => {
